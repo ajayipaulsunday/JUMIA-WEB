f934a1cf6bd93ea92849a18a1dfb2e3d1f168ecc
Ajayi Paul <ajayipaulfela@gmail.com>
2023-07-03 23:19:45 -0700

    jumia adjusted

--- index.js
+++ index.js
@@ -1,48 +1,53 @@
-const swiper = new Swiper('.sample-slider', {
+const swiper = new Swiper(".sample-slider", {
   // loop: true,
-  slidesPerView: 2,      
+  slidesPerView: 2,
   // autoplay: {
   //     delay: 1,
   // },
-}) 
- let allAccount = JSON.parse(localStorage.getItem("Details"))
- console.log(allAccount);
+});
+let allAccount = JSON.parse(localStorage.getItem("Details"));
+console.log(allAccount);
 
-  let cartItems =JSON.parse(localStorage.getItem("cartItems"))
-  let account = JSON.parse(localStorage.getItem("Details"))     
+let cartItems = JSON.parse(localStorage.getItem("cartItems"));
+let account = JSON.parse(localStorage.getItem("Details"));
 
-  // let userItem =  cartItems.filter((item, index, array)=> item.email == account.Email)
+// let userItem =  cartItems.filter((item, index, array)=> item.email == account.Email)
 
- //  console.log(user);
-   
-  //  for (let index = 0; index < cartItems.length; index++) {
-  //    counter.innerHTML = cartItems.length
-  //   }
-     
-  // counter.innerText=JSON.parse(localStorage.getItem("cartItems")).length
-function savetoLocal(myImg, price, productName,beforePrice){
+//  console.log(user);
+
+//  for (let index = 0; index < cartItems.length; index++) {
+//    counter.innerHTML = cartItems.length
+//   }
+
+// counter.innerText=JSON.parse(localStorage.getItem("cartItems")).length
+function savetoLocal(myImg, price, productName, beforePrice) {
   let map = {
-    myImg, price, productName, beforePrice
-  }
-  localStorage.setItem("saveItems", JSON.stringify(map))
+    myImg,
+    price,
+    productName,
+    beforePrice,
+  };
+  localStorage.setItem("saveItems", JSON.stringify(map));
   console.log(map);
-  window.location.href ="addToCart.html"
+  window.location.href = "addToCart.html";
 }
-function savetoLocals(myImg, price, productName,beforePrice){
+function savetoLocals(myImg, price, productName, beforePrice) {
   let map = {
-    myImg, price, productName, beforePrice
-  }
-  localStorage.setItem("saveItems", JSON.stringify(map))
-  console.log(map);
-  // window.location.href ="nAccount.html"
-  window.location.href ="addToCart.html"
+    myImg,
+    price,
+    productName,
+    beforePrice,
+  };
+  localStorage.setItem("saveItems", JSON.stringify(map));
+  window.location.href = "dontHaveAccount.html";
 }
-display.innerHTML =`
+display.innerHTML = `
 <p>${allAccount.FirtName}</p>
-`
-let userItem =  cartItems.filter((item, index, array)=> item.email == account.Email)
+`;
+let userItem = cartItems.filter(
+  (item, index, array) => item.email == account.Email
+);
 for (let index = 0; index < userItem.length; index++) {
   // counter.innerHTML = userItem.length
-  showCart.innerHTML = userItem.length
-
+  showCart.innerHTML = userItem.length;
 }
